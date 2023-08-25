a9ed2166c85efe3cf21e96bf73fc52c1ce533f98
AkramChawki <akramchawki01@gmail.com>
2023-08-25 14:38:47 +0100

    fixed detaills

--- resources/views/filament/resources/inventaire-resource/pages/cuisinier-inventaire-details.blade.php
+++ resources/views/filament/resources/inventaire-resource/pages/cuisinier-inventaire-details.blade.php
@@ -5,11 +5,58 @@
         <div class="max-w-3xl mx-auto">
             <h1 class="font-bold mb-4 text-3xl">Nom: {{ $record->name }}</h1>
             <p class="mb-4 text-xl font-medium">Les produits: </p>
-            <ul class="list-disc pl-6 text-lg">
-                @foreach ($record->products() as $product)
-                    <li>{{ $product->designation }}, Quantité: {{ $product->qty }}</li>
-                @endforeach
-            </ul>
+            <div class="flex flex-col">
+                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead class="bg-gray-50">
+                                    <tr>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            produit</th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            designation</th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            quantité</th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            unité</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="bg-white divide-y divide-gray-200">
+                                    @foreach ($record->products() as $product)
+                                        <tr>
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                <div class="flex items-center">
+                                                    <div class="flex-shrink-0 h-10 w-10">
+                                                        <img class="h-10 w-10 rounded-full"
+                                                            src="{{ asset('storage/' . $product->image) }}"
+                                                            alt="">
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                <div class="text-sm text-gray-900">{{ $product->designation }}</div>
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                <span
+                                                    class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                                    {{ $product->qty }} </span>
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                {{ $product->unite }}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </main>
 
